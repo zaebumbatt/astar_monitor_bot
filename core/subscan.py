9bb3199de58c22e58c66651510a1eda45c3f20b9
Nikita Ustiuzhanin <zaebumba@gmail.com>
2024-02-08 08:40:06 +0700

    Switch to V2 API

--- core/subscan.py
+++ core/subscan.py
@@ -6,15 +6,11 @@ from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
 SUBSCAN_API_KEY = os.getenv('SUBSCAN_API_KEY')
-SUBSCAN_API_URL = 'https://astar.api.subscan.io/api/scan/'
 SUBSCAN_API_V2_URL = 'https://astar.api.subscan.io/api/v2/scan/'
 
 
 def make_request(resource: str, data: dict) -> [dict, None]:
-    url = (
-              SUBSCAN_API_V2_URL if resource != 'extrinsics'
-              else SUBSCAN_API_URL
-          ) + resource
+    url = SUBSCAN_API_V2_URL + resource
     headers = {"x-api-key": SUBSCAN_API_KEY}
     response = requests.post(url, headers=headers, json=data)
     logger.info(f'make_request: {url} {response.status_code}')
